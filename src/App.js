ec75aec6454e2ab9d62c0a791b6565ba069d6260
Dinesh <dinabeysinghe@gmail.com>
2023-10-11 21:44:20 +0530

    call API in normaly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  //Normal API call 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+      })
+      .catch(e => {
+        console.log(e)
+      });
+
+  }, []);
+
+
+  //Axio API call 
+  useEffect(() => { }, [
+
+  ]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>HI</h1>
     </div>
   );
 }
