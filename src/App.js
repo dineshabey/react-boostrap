0c8e4fe037229529073852daf5ac214db8a8c395
Dinesh <dinabeysinghe@gmail.com>
2023-10-11 22:15:20 +0530

    API POST using AXIO

--- src/App.js
+++ src/App.js
@@ -8,36 +8,51 @@ import axios from 'axios';
 function App() {
 
   //Normal API call 
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos/')
+  //     .then(response => response.json())
+  //     .then(json => {
+  //       console.log(json)
+  //     })
+  //     .catch(e => {
+  //       console.log(e)
+  //     });
+
+  // }, []);
+
+
+  //Axio GET API call 
+  // useEffect(() => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/')
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .finally(function () {
+  //       // always executed
+  //     });
+  // }, [
+  // ]);
+
+
+  //Axio post API call
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then(response => response.json())
-      .then(json => {
-        // console.log(json)
-      })
-      .catch(e => {
-        console.log(e)
-      });
-
-  }, []);
-
-
-  //Axio API call 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos/')
+    axios.post('https://jsonplaceholder.typicode.com/posts', {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    })
       .then(function (response) {
-        // handle success
         console.log(response.data);
       })
       .catch(function (error) {
-        // handle error
         console.log(error);
-      })
-      .finally(function () {
-        // always executed
       });
-  }, [
-
-  ]);
+  }, []);
 
 
   return (
